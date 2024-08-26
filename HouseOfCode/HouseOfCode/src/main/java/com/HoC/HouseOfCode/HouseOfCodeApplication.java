9d8d138a5aed86d0e5bc3e1e7928c84d9f39fe2d
Sandeep Singh <sandeepgaurdec13@gmail.com>
2024-08-26 20:55:15 +0530

    Started House of Code

---
+++ HouseOfCode/HouseOfCode/src/main/java/com/HoC/HouseOfCode/HouseOfCodeApplication.java
@@ -0,0 +1,13 @@
+package com.HoC.HouseOfCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseOfCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseOfCodeApplication.class, args);
+	}
+
+}
---
+++ HouseOfCode/HouseOfCode/src/test/java/com/HoC/HouseOfCode/HouseOfCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HoC.HouseOfCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseOfCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
